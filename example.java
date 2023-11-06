d4aba797ad836761e7727ed12d62dba17844df9e
simon marchal <simon.marchal@epitech.eu>
2023-11-06 05:00:05 -0500

    update

--- example.java
+++
@@ -1,7 +0,0 @@
-public class example {
-        public static void main ( String [] args ) {
-            Food bread = new SoftBread () ;
-            System . out . println ( " The softbread costs " + bread . getPrice () +
-                    " euros and contains " + bread . getCalories () + " calories . " ) ;
-        }
-    }
